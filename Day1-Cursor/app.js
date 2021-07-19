a03b91307727f31f4973b1eafccedd1b3d162b1c
Harshvardhan Shete <hvshete6@gmail.com>
2021-07-20 00:39:51 +0530

    First Commit

---
+++ Day1-Cursor/app.js
@@ -0,0 +1,27 @@
+const cursor = document.querySelector(".cursor");
+
+window.addEventListener("mousemove", (e) => {
+  cursor.style.left = e.pageX + "px";
+  cursor.style.top = e.pageY + "px";
+  cursor.setAttribute("data-fromTop", cursor.offsetTop - scrollY);
+  // console.log(e.pageX,e.pageY)
+});
+
+window.addEventListener("scroll", () => {
+  const fromTop = parseInt(cursor.getAttribute("data-fromTop"));
+  cursor.style.top = scrollY + fromTop + "px";
+
+  console.log(scrollY);
+});
+
+window.addEventListener("click", () => {
+  if (cursor.classList.contains("click")) {
+    cursor.classList.remove("click");
+
+    // Trigerring a DOM reflow
+    void cursor.offsetWidth;
+    cursor.classList.add("click");
+  } else {
+    cursor.classList.add("click");
+  }
+});
